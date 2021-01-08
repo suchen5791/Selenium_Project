319fe9172b2755af441c9a83d4079fefe5ccc91f
Suchen <34927399+suchen5791@users.noreply.github.com>
2021-01-07 21:51:31 -0500

    ExcelDataDrivenDay2

---
+++ MavenBuild/src/test/java/dataDriven/DataWriterByColumnName.java
@@ -0,0 +1,45 @@
+package dataDriven;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataWriterByColumnName 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		FileInputStream fis = new FileInputStream("D:\\SELENIUM\\xldata_test\\testdata1.xlsx");
+		FileOutputStream fos = null;
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("login");
+		XSSFRow row = sheet.getRow(0);
+		XSSFCell cell = null;
+		
+		int colNum = -1; 
+		for(int i=0;i<row.getLastCellNum();i++)
+		{
+			if(row.getCell(i).getStringCellValue().trim().equals("Result"))
+				colNum = i;
+				
+		}
+		
+		row = sheet.getRow(3);
+		cell = row.getCell(colNum);
+		cell.setCellValue("Skipped");
+		
+
+		fos = new FileOutputStream("D:\\SELENIUM\\xldata_test\\testdata1.xlsx");
+		wb.write(fos);
+		wb.close();
+		fos.close();
+		
+
+	}
+
+}
